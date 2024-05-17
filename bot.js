b6f9d853bcd3417442c1ef6cc89a233413c51d77
Michilis <120072772+Michilis@users.noreply.github.com>
2024-05-17 19:23:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const { Telegraf } = require('telegraf');
 const dotenv = require('dotenv');
 const {
     handleStart,
-    handleCreateWallet,
     handleReceiveToken,
     handleSendToken,
     handleCheckBalances,
@@ -15,12 +14,17 @@ dotenv.config();
 const bot = new Telegraf(process.env.TELEGRAM_BOT_API_KEY);
 
 bot.start(handleStart);
-bot.command('create_wallet', handleCreateWallet);
 bot.command('receive_token', handleReceiveToken);
 bot.command('send_token', handleSendToken);
 bot.command('check_balances', handleCheckBalances);
 bot.command('pay_ln_invoice', handlePayLnInvoice);
 bot.command('help', handleHelp);
+
+bot.action('receive_token', (ctx) => ctx.reply('Please send your encoded token.'));
+bot.action('send_token', (ctx) => ctx.reply('Please send the amount and tokens.'));
+bot.action('check_balances', handleCheckBalances);
+bot.action('pay_ln_invoice', (ctx) => ctx.reply('Please send the Lightning invoice.'));
+bot.action('help', handleHelp);
 
 bot.catch((err, ctx) => {
     console.error(`Error for ${ctx.updateType}`, err);
