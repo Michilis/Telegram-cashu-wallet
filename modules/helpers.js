10b9216d32466ff3fab629aad0c5469d617d3a41
Michilis <120072772+Michilis@users.noreply.github.com>
2024-05-17 19:19:08 +0200

    Update helpers.js

--- modules/helpers.js
+++ modules/helpers.js
@@ -1,13 +1,21 @@
-const { Telegraf } = require('telegraf');
+const { Markup } = require('telegraf');
 
-const createYesNoButtons = (text, yesCallback, noCallback) => {
-    return Telegraf.Extra.markup((markup) => {
-        return markup.inlineKeyboard([
-            [markup.callbackButton('Yes', yesCallback), markup.callbackButton('No', noCallback)]
-        ]).resize();
-    });
+const createInlineKeyboard = (buttons) => {
+    return Markup.inlineKeyboard(buttons.map(button => Markup.button.callback(button.text, button.callback_data)));
 };
 
+const mainMenuButtons = [
+    { text: 'Create Wallet', callback_data: 'create_wallet' },
+    { text: 'Receive Token', callback_data: 'receive_token' },
+    { text: 'Send Token', callback_data: 'send_token' },
+    { text: 'Check Balances', callback_data: 'check_balances' },
+    { text: 'Pay LN Invoice', callback_data: 'pay_ln_invoice' },
+    { text: 'Help', callback_data: 'help' },
+];
+
+const createMainMenu = () => createInlineKeyboard(mainMenuButtons);
+
 module.exports = {
-    createYesNoButtons,
+    createInlineKeyboard,
+    createMainMenu,
 };
